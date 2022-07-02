b4f1b8184bbd577b50c45fd47a64ddc4d18dc94d
Joe Pigott <joe.pigott17@gmail.com>
2022-07-02 17:33:20 -0600

    Added example for sqrt < 1

--- examples/test.c
+++ examples/test.c
@@ -3,6 +3,7 @@
 
 int main() {
     printf("pig_sqrt(10): %f\n", pig_sqrt(10));
+    printf("pig_sqrt(0.5f): %f\n", pig_sqrt(0.5f));
     printf("pig_sqrt(-10): %f\n", pig_sqrt(-10));
     printf("pig_floor(8.75f): %d\n", pig_floor(8.75f));
     printf("pig_ceil(8.75f): %d\n", pig_ceil(8.75f));
